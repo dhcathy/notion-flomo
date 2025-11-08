caa3760790ed1214bbaf80f3a23f00a7cd3a674e
Cathy He <161542441+dhcathy@users.noreply.github.com>
2025-11-08 10:16:45 +0800

    Refactor database query method in notion_helper.py

--- notionify/notion_helper.py
+++ notionify/notion_helper.py
@@ -84,7 +84,7 @@ class NotionHelper:
         has_more = True
         start_cursor = None
         while has_more:
-            response = self.client.databases.query_database(
+            response = self.client.databases.query(
                 database_id=database_id,
                 start_cursor=start_cursor,
                 page_size=100,
